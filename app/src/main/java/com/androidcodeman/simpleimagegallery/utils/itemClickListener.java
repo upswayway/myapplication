ec75595159138680c1d1494c4d63b9838e1d29b1
meetdhameliya18e <meetdhameliya@aanibrothers.in>
2023-07-14 11:07:08 +0530

    first commit

---
+++ app/src/main/java/com/androidcodeman/simpleimagegallery/utils/itemClickListener.java
@@ -0,0 +1,11 @@
+package com.androidcodeman.simpleimagegallery.utils;
+
+import java.util.ArrayList;
+
+
+public interface itemClickListener {
+
+
+    void onPicClicked(PicHolder holder, int position, ArrayList<pictureFacer> pics);
+    void onPicClicked(String pictureFolderPath,String folderName);
+}
